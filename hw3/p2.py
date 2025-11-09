41f9352853710bf4d44d57fe54ba7b5af088c0fb
gandres42 <gandres2416@gmail.com>
2025-11-09 10:59:52 -0800

    problem 4, update deps

--- hw3/p2.py
+++ hw3/p2.py
@@ -2,16 +2,26 @@ import modern_robotics as mr
 import numpy as np
 import math
 
-# h = 0.20
-# r = 0.02
-# m = math.pi * math.pow(r, 2) * h * 7500
+h = 0.20
+r = 0.02
+m = math.pi * math.pow(r, 2) * h * 7500
+print(m)
 
-# I = m * (((3 * math.pow(r, 2)) + math.pow(h, 2)) / 12)
-# I = (m * math.pow(r, 2)) / 2
-# print(I)
+# # I = m * (((3 * math.pow(r, 2)) + math.pow(h, 2)) / 12)
+# # I = (m * math.pow(r, 2)) / 2
+# # print(I)
 
-r = 0.1
-m = 7500 * ((4/3) * math.pi * math.pow(r, 3))
-I = m * (math.pow(r, 2) + math.pow(r, 2))/5
+# # I = np.array([
+# #     [0.1256, 0, 0],
+# #     [0, 0.1256, 0],
+# #     [0., 0, .000376]
+# # ])
 
-print(m * math.pow(0.1, 2))
+# r = 0.1
+# m = 7500 * ((4/3) * math.pi * math.pow(r, 3))
+# dI = m * math.pow(0.2, 2)
+# print(dI)
+
+# # print(I + dI + dI)
+# # # print(m * math.pow(0.1, 2))
+
