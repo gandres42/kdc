8c0b9ac1d4972981a3d12514f64d6642f6c4b82e
gandres42 <gandres2416@gmail.com>
2025-11-24 16:48:29 -0800

    added comments, code cleanup

--- hw4/sim.py
+++ hw4/sim.py
@@ -20,30 +20,13 @@ class Arm:
         self.I2 = I2
         self.g = g
     
-    def inverse_dynamics(self, state: ArmState):
-        theta1, theta2 = state.position
-        dtheta1, dtheta2 = state.velocity
-        ddtheta1, ddtheta2 = state.acceleration
-        
-        M = np.matrix([
-            [(self.m1 * np.pow(self.r1, 2)) + self.I1 + self.I2 + (self.m2 * (np.pow(self.L, 2) + np.pow(self.r2, 2) + (2 * self.L * self.r2 * np.cos(theta2)))), (self.m2 * self.L * self.r2 * np.cos(theta2)) + (self.m2 * np.pow(self.r2, 2)) + self.I2],
-            [(self.m2 * np.pow(self.r2, 2)) + (self.m2 * self.L * self.r2 * np.cos(theta2)) + self.I2, (self.m2 * np.pow(self.r2, 2)) + self.I2]
-        ])
-        c = np.array([
-            [-self.m2 * self.L * self.r2 * dtheta2 * (2 * dtheta1 + dtheta2) * np.sin(theta2)],
-            [self.m2 * self.L * self.r2 * np.pow(dtheta1, 2) * np.sin(theta2)]
-        ])
-        g_vec = np.array([
-            [self.g * ((self.m1 * self.r1 * np.sin(theta1)) + (self.m2 * self.L * np.sin(theta1)) + (self.m2 * self.r2 * np.sin(theta1 + theta2)))],
-            [self.g * self.m2 * self.r2 * np.sin(theta1 + theta2)]
-        ])
-        
-        ddtheta_vec = np.array([[ddtheta1], [ddtheta2]])
-        tau = np.dot(M, ddtheta_vec) + c + g_vec
-        
-        return tau
-    
     def forward_dynamics(self, state: ArmState, torques):
+        """
+        Computes joint accelerations based on current arm state and applied torques.  Uses dynamics from class slides.
+
+        :param state: current arm state
+        :param torques: applied joint torques
+        """
         theta1, theta2 = state.position
         dtheta1, dtheta2 = state.velocity
         tau1, tau2 = torques
@@ -66,36 +49,58 @@ class Arm:
         
         return ddtheta.flatten()
     
-    def plan(self, start, goal, plan_time=2.0) -> list[ArmState]:
-        # number of timestemps
-        n_steps = int(plan_time / dt)
+    def trajectory(self, start: ArmState, goal: ArmState, T) -> list[ArmState]:
+        """
+        Generates 5th order polynomial trajectory from start to start state to goal state in time T.
+
+        :param start: starting arm state
+        :param goal: desired ending arm state
+        :param T: duration of arm movement from start to goal
+
+        """
+        n_steps = int(T / dt)
         
-        # Initialize list of states
         states = []
         
-        # Generate trajectory for each timestep
+        pf = np.array(goal.position)
+        vf = np.array(goal.velocity)
+        af = np.array(goal.acceleration)
+        c0 = np.array(start.position)
+        c1 = np.array(start.velocity)
+        c2 = np.array(start.acceleration) / 2
+
+        A = np.array([
+            [T**3, T**4, T**5],
+            [3*T**2, 4*T**3, 5*T**4],
+            [6*T, 12*T**2, 20*T**3]
+        ])
+        
+        b = np.array([
+            pf - c0 - c1*T - c2*T**2,
+            vf - c1 - 2*c2*T,
+            af - 2*c2
+        ])
+        
+        coeffs = np.linalg.solve(A, b)
+        c3 = coeffs[0]
+        c4 = coeffs[1]
+        c5 = coeffs[2]
+        
         for i in range(n_steps):
-            t = i * dt
-            s = t / plan_time  # Normalized time [0, 1]
-            
-            # 5th order polynomial: 10s^3 - 15s^4 + 6s^5
-            # This gives s(0)=0, s(1)=1 with zero velocity and acceleration at endpoints
-            s_t = 10 * s**3 - 15 * s**4 + 6 * s**5
-            ds_t = (30 * s**2 - 60 * s**3 + 30 * s**4) / plan_time
-            dds_t = (60 * s - 180 * s**2 + 120 * s**3) / (plan_time**2)
-            
-            # Compute position, velocity, acceleration for both joints
-            start_arr = np.array(start)
-            goal_arr = np.array(goal)
-            positions = start_arr + (goal_arr - start_arr) * s_t
-            velocities = (goal_arr - start_arr) * ds_t
-            accelerations = (goal_arr - start_arr) * dds_t
+            t = i * dt    
+            # compute position, velocity, acceleration using quintic polynomial
+            positions = c0 + c1*t + c2*t**2 + c3*t**3 + c4*t**4 + c5*t**5
+            velocities = c1 + 2*c2*t + 3*c3*t**2 + 4*c4*t**3 + 5*c5*t**4
+            accelerations = 2*c2 + 6*c3*t + 12*c4*t**2 + 20*c5*t**3
             
             states.append(ArmState(positions, velocities, accelerations))
         
         return states
 
     def embiggen(self):
+        """
+        Increases arm weights by 20%.
+        """
         self.m1 = self.m1 * 1.2
         self.m2 = self.m2 * 1.2
 
@@ -171,13 +176,16 @@ class Sim:
         return [x0, x1, x2], [y0, y1, y2]
     
     def get_state(self):
-        return ArmState(np.array([self.theta1, self.theta2]), np.array([self.omega1, self.omega2]), np.array([self.alpha1, self.alpha2]))
+        return ArmState(
+            np.array([self.theta1, self.theta2]),
+            np.array([self.omega1, self.omega2]),
+            np.array([self.alpha1, self.alpha2])
+        )
     
-    def set_state(self, theta1, theta2, omega1=0.0, omega2=0.0):
-        self.theta1 = theta1
-        self.theta2 = theta2
-        self.omega1 = omega1
-        self.omega2 = omega2
+    def set_state(self, state: ArmState):
+        self.theta1, self.theta2 = state.position
+        self.omega1, self.omega2 = state.velocity
+        self.alpha1, self.alpha2 = state.acceleration
         
     def draw(self, show_trace=True):
         x, y = self.get_positions()
@@ -199,38 +207,64 @@ sim = Sim()
 arm = Arm()
 
 # generate motion plan
-states = arm.plan((-np.pi/4, 0), (np.pi/4, np.pi/2), 2.0)
-sim.set_state(-np.pi/4, 0)
+start_state = ArmState(
+    np.array([-np.pi/4, 0]),
+    np.array([0, 0]),
+    np.array([0, 0])
+)
+target_state = ArmState(
+    np.array([np.pi/4, np.pi/2]),
+    np.array([0, 0]),
+    np.array([0, 0])
+)
+duration = 2.00
+
+states = arm.trajectory(start_state, target_state, duration)
+sim.set_state(start_state)
 
 # make link masses 20% bigger
 arm.embiggen()
 
 # pid controller gains
-Kp = np.array([800.0, 800.0])
-Kd = np.array([100.0, 100.0])
-Ki = np.array([50.0, 50.0])
+factor = 120
+Kp = np.array(np.full(2, 8 * factor))
+Ki = np.array(np.full(2, 4 * factor))
+Kd = np.array(np.full(2, 1 * factor))
+
 integral_error = np.array([0.0, 0.0])
 
 t = 0
-for target_state in states:
+for state in states:
     t += dt
-    print(f"time: {round(t, 1)}/2.0      ", end="\r")
+    print(f"time: {t:.2f}/{duration:.2f}", end="\r")
     if sim.running:
         # get current state
         current_state = sim.get_state()
         
         # compute error
-        position_error = target_state.position - current_state.position
-        velocity_error = target_state.velocity - current_state.velocity
+        position_error = state.position - current_state.position
+        velocity_error = state.velocity - current_state.velocity
         integral_error += position_error * sim.dt
         
         # update torques based on pid error
-        controls = Kp * position_error + Kd * velocity_error + Ki * integral_error
+        torques = Kp * position_error + Kd * velocity_error + Ki * integral_error
         
         # update sim with forward dynamics
-        accels = arm.forward_dynamics(current_state, controls)
+        accels = arm.forward_dynamics(current_state, torques)
         sim.update_position(accels)
         sim.draw()
     else:
         break
 print()
+final_state = sim.get_state()
+
+# Error summary
+print(f"Target Position:  [{target_state.position[0]:.4f}, {target_state.position[1]:.4f}]")
+print(f"Final Position:   [{final_state.position[0]:.4f}, {final_state.position[1]:.4f}]")
+position_error = target_state.position - final_state.position
+print(f"Position Error:   [{position_error[0]:.4f}, {position_error[1]:.4f}]")
+print("-" * 50)
+print(f"Target Velocity:  [{target_state.velocity[0]:.4f}, {target_state.velocity[1]:.4f}]")
+print(f"Final Velocity:   [{final_state.velocity[0]:.4f}, {final_state.velocity[1]:.4f}]")
+velocity_error = target_state.velocity - final_state.velocity
+print(f"Velocity Error:   [{velocity_error[0]:.4f}, {velocity_error[1]:.4f}]")
